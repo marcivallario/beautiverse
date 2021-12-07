c3f8c1f0d94d7001e0a86bf16aae6f8296fc6cea
Marci Vallario <marcivallario@gmail.com>
2021-12-07 17:38:48 -0500

    Added render favorites list functions

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ let productData;
 let featuredProduct;
 let results = document.getElementById('results');
 let form = document.getElementById('search-bar');
-
+let favoritesList = document.getElementById('favorites-list');
+let currentProduct;
 
 document.addEventListener('DOMContentLoaded', () => {
     fetch('http://makeup-api.herokuapp.com/api/v1/products.json')
@@ -14,8 +15,11 @@ document.addEventListener('DOMContentLoaded', () => {
         buildProductCard(featuredProduct);
     })
 
-    form.addEventListener('submit', getSearchResults)
+    fetch('http://localhost:3000/favorites')
+    .then(resp => resp.json())
+    .then(favoritesListData => renderFavoritesList(favoritesListData))
 
+    form.addEventListener('submit', getSearchResults)
 })
 
 function buildProductCard(product) {
@@ -26,21 +30,22 @@ function buildProductCard(product) {
     shadeNames = shadeNames.join(', ');
     card.className = 'makeup-details';
     card.innerHTML = `
-    <img class='makeup-image' src='${product.api_featured_image}' />
-                <div class='makeup-text'>
-                    <h2 class='makeup-name'>${product.name}</h2>
-                    <span id="favorite-button" class="iconify" data-icon="bi:bookmark-heart"></span>
-                    <p>${product.description}</p>
-                    <ul em class='product-description'>
-                    <br>
-                        <li>Price: ${(product.price_sign ? product.price_sign : '$')
-                        } ${(parseFloat(product.price).toFixed(2))}</li>
-                        <li>Available shades: ${shadeNames}</li>
-                        <li>Buy at <a href=${product.product_link} target='_blank'>${product.brand}</a></li>
-                    </ul>
-                </div>
+        <img class='makeup-image' src='${product.api_featured_image}' />
+        <div class='makeup-text'>
+            <h2 class='makeup-name'>${product.name}</h2>
+            <button class="favorite-button">Favorite</button>
+            <p>${product.description}</p>
+            <ul em class='product-description'>
+            <br>
+                <li>Price: ${(product.price_sign ? product.price_sign : '$')
+                } ${(parseFloat(product.price).toFixed(2))}</li>
+                <li>Available shades: ${shadeNames}</li>
+                <li>Buy at <a href=${product.product_link} target='_blank'>${product.brand}</a></li>
+            </ul>
+        </div>
     `
     results.append(card);
+    // document.querySelector('.favorite-button').addEventListener('click', addToFavorites);
 }
 
 function getSearchResults(e) {
@@ -64,4 +69,30 @@ function getSearchResults(e) {
 
 function loadSearchResults(productArray) {
     productArray.forEach(product => buildProductCard(product))
+}
+
+// function addToFavorites(product) {
+    
+//     fetch('http://localhost:3000', {
+//         method: 'POST',
+//         headers: {
+//             'Content-Type': 'application/json'
+//         },
+//         body: JSON.stringify(product)
+//     })
+//     // pass product info into this function
+//     // send fetch POST request to db.json with this information
+//     // run renderfavoriteslist function (that has helper of renderfavoriteditem)
+
+// }
+
+function renderFavoritesList(favoritesListArr) {
+    favoritesList.innerHTML = '';
+    favoritesListArr.forEach(favorite => renderFavorite(favorite))
+}
+
+function renderFavorite(favorite) {
+    let li = document.createElement('li');
+    li.textContent = favorite.name;
+    favoritesList.append(li);
 }
